b7aeb72b271a9d6e9af671d3e840463c2d7078eb
Alejandro Montilla <amontilla@luckycycle.com>
2022-09-13 15:16:01 +0200

    Change variable name

--- assessment3/main.js
+++ assessment3/main.js
@@ -4,7 +4,7 @@ import { createQuestion } from './modules/question.component.js';
 (async () => {
 	const question = await fetchQuestion();
 
-	const $question = await createQuestion(question);
+	const $question = createQuestion(question);
 
 	const $app = document.getElementById('app');
 	$app.innerHTML = '';
