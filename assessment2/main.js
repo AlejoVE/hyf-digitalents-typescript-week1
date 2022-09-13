42dc1d35587493e9d334ed71bb41885f76f62807
Alejandro Montilla <amontilla@luckycycle.com>
2022-09-13 15:26:07 +0200

    Change variable name

--- assessment2/main.js
+++ assessment2/main.js
@@ -1,13 +1,13 @@
 import { encryptText } from './modules/encrypt.js';
 
 const txtEl = document.querySelector('#plaintext');
-const encrypTxtEl = document.querySelector('#encryptedtext');
+const encryptTxtEl = document.querySelector('#encryptedtext');
 const shiftEl = document.querySelector('#shift');
 
 txtEl.addEventListener('keyup', () => {
-	const number = Number(shiftEl.value);
-	if (number) {
-		const newText = encryptText(txtEl.value, number);
-		encrypTxtEl.innerHTML = newText;
+	const shiftValue = Number(shiftEl.value);
+	if (shiftValue) {
+		const newText = encryptText(txtEl.value, shiftValue);
+		encryptTxtEl.innerHTML = newText;
 	}
 });
